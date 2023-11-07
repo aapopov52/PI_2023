5c9d3d3fff79dd4bfa3cc5297905be8682bef036
Anton <antoshka.novikov2001@mail.ru>
2023-11-07 18:48:16 +0300

    novikov.py

---
+++ novikov.py
@@ -0,0 +1,7 @@
+from transformers import pipeline
+
+model_name = "IlyaGusev/rubertconv_toxic_clf"
+pipe = pipeline("text-classification", model=model_name, tokenizer=model_name, framework="pt")
+
+text = "Ты придурок из интернета"
+pipe([text])
